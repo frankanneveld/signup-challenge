00d389140468eecd9311c903d50c232f22c38baf
Frank Anneveld <frank@anneveld.it>
2021-02-10 16:18:09 +0100

    Added error message for email validation

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,7 +12,7 @@ export class SignupComponent {
 
   /**
    * Declaration for the form to be controlled, email validation is a more common regular expression than the
-   * build in validator
+   * build in validator from Angular it self
    */
   public form: FormGroup = new FormGroup({
     firstname: new FormControl('', [Validators.required]),
@@ -22,6 +22,20 @@ export class SignupComponent {
   });
 
   constructor(private readonly apiService: ApiService) {}
+
+  /**
+   * Gets the email directly from the form
+   */
+  public get email(): FormControl {
+    return this.form.get('email') as FormControl;
+  }
+
+  /**
+   * Gets the password directly from the form
+   */
+  public get password(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
 
   /**
    * A custom password validator with requirements :
