a21bff24a26d86e73c49a596da567835242df1f0
Frank Anneveld <frank@anneveld.it>
2021-02-10 21:11:58 +0100

    Added http response error

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from '../../environments/environment'
 import { SignUp } from '@models/signup.model';
 
@@ -12,6 +13,18 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   public post(message: SignUp): Observable<any> {
-    return this.http.post<SignUp>(environment.api_endpoint, message);
+    return this.http.post<SignUp>(environment.api_endpoint, message).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    return throwError('Something bad happened; please try again later.');
   }
 }
